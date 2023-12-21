320af254ac74763d0108b8d16d79a2cbd3270c85
HBous <122835950+HOUBOUS@users.noreply.github.com>
2023-12-21 10:20:27 +0100

    Configuration authReducer

--- front/src/Redux/reducers/auth.reducer.js
+++ front/src/Redux/reducers/auth.reducer.js
@@ -1,11 +1,25 @@
+export const USER_SIGNIN_SUCCESS = 'USER_SIGNIN_SUCESS';
+export const USER_SIGNIN_FAIL = 'USER_SIGNIN_FAIL';
+export const USER_SIGNOUT = 'USER_SIGNOUT';
+
+
 
 const initialState = { 
-    use: '',
+    user: '',
+    token: '',
+    isLogged: false,
 
 };
 
 export default function authReducer ( state= initialState, action){
-
+   switch(action.type){
+    case USER_SIGNIN_SUCCESS:
+        return { isLogged: true, token:action.payload.body.token }
+    case USER_SIGNIN_FAIL: 
+        return { isLogged: false, token: null, error: action.paylaod }
+    case USER_SIGNOUT: 
+        return { isLogged: false, token: null }
+        default: 
     return state;
-
+}
 };
