661e61a536f09185eeefeb6d5002fe460795bbdf
HBous <122835950+HOUBOUS@users.noreply.github.com>
2024-01-09 15:17:33 +0100

    UpDate Swagger.yaml doc

--- argent-bank-frontend/src/components/Nav.jsx
+++ argent-bank-frontend/src/components/Nav.jsx
@@ -1,14 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../redux/loginSlice"
+import { logout } from "../redux/loginSlice";
 
 export default function Nav() {
   const dispatch = useDispatch();
-    const { isLogged } = useSelector((state) => state.loginReducer);
-    const user = useSelector((state) => state.userReducer.user);
-    const userName = user ? user.userName : '';
+  const { isLogged } = useSelector((state) => state.loginReducer);
+  const user = useSelector((state) => state.userReducer);
+  // const userName = user ? user.userName : '';
 
-    return (
+  return (
     <nav className="main-nav">
       <a className="main-nav-logo" href="./">
         <img
@@ -19,12 +19,17 @@ export default function Nav() {
         <h1 className="sr-only">Argent Bank</h1>
       </a>
       <div>
-         {isLogged && (
+        {isLogged && (
           <>
             <Link className="main-nav-item" to="/user">
-              <i className="fa fa-user"></i> {userName}
+              <i className="fa fa-user"></i> 
+              {/* {userName} */}
             </Link>
-            <Link className="main-nav-item" to="./login" onClick={() => dispatch(logout())}>
+            <Link
+              className="main-nav-item"
+              to="./login"
+              onClick={() => dispatch(logout())}
+            >
               <i className="fa fa-sign-out"></i> Sign Out
             </Link>
           </>
@@ -36,5 +41,5 @@ export default function Nav() {
         )}
       </div>
     </nav>
-    )
+  );
 }
